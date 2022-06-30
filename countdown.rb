11e4ca1f1a44a24eabdc1f814a6313e322d85378
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-06-30 15:11:15 -0400

    commit tree 2afd112b76c2ce45e1533a9df638e6c6ca3b4dab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown (i)
+    while i > 0
+        puts "#{i} SECOND(S)!"
+     i -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds_to_midnight)
+    sleep(5)
+    seconds_to_midnight -= 1
+end
+
